5e1dd9e1e4ea043ab101d02f93b1a1a0af1fd2c5
Mohammad Alfayoume <mabed3819@gmail.com>
2022-09-26 08:50:14 +0300

    fetch the products and product from back-end

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React from "react";
-import HomeSecreen from "./secreens/HomeSecreen";
-import { Container } from "react-bootstrap";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import HomeSecreen from './secreens/HomeSecreen'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import ProductSecreen from './secreens/ProductSecreen'
 
-const App=()=> {
+const App = () => {
   return (
-    <>
-    <Header />
-    <main className="py-3">
-      <Container>
-      <HomeSecreen />
-      </Container>
-    </main>
-    <Footer />
-    </>
-  );
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomeSecreen />} exact />
+            <Route path='/product/:id' element={<ProductSecreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
--- src/secreens/HomeSecreen.js
+++ src/secreens/HomeSecreen.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
-import products from '../products'
 import Product from '../components/Product'
+import axios from 'axios'
 
 const HomeSecreen = () => {
+  const [products, setProducts] = useState([]) //we use useState because it works like the constructor
+
+  // we use useEffect it excecuted when component loades or page refresh //it similer to componentDidMount() in class component
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products') //we have to add proxy in the package.json to look at http://localhost:5000 rather than looking to http://localhost:3000, so we added "proxy": "http://127.0.0.1:5000"
+
+      setProducts(data)
+    }
+    fetchProducts()
+  }, [])
   return (
     <>
       <h1>Latest Products</h1>
       <Row>
         {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3} key={product._id}> adding key to the element to make it unique
-            <Product product={product}/>
+          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+            {' '}
+            {/*adding key to the element to make it unique*/}
+            <Product product={product} />
           </Col>
         ))}
       </Row>
